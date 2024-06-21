606007cf16ecfd4f005d4f7c2e08d151a3d99b31
Maiko Tan <maiko.tan.coding@gmail.com>
2024-06-21 11:18:37 +0800

    fix: import resolver for typescript

--- typescript.js
+++ typescript.js
@@ -7,13 +7,14 @@ module.exports = {
   extends: ['@hamster-bot/eslint-config/typescript'],
   plugins: ['import'],
   settings: {
-    // eslint-disable-next-line n/global-require
-    ...require('./typescript').settings,
     'import/extensions': ['.cjs', '.js', '.mjs', '.jsx', 'cts', '.ts', '.mts', '.tsx'],
     'import/parser': {
       '@typescript-eslint/parser': ['.cts', '.ts', '.mts', '.tsx'],
     },
     'import/ignore': ['node_modules'],
+    'import/resolver': {
+      typescript: {},
+    },
   },
   rules: {
     'import/no-unresolved': ['error'],
