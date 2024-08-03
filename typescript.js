a3273e647df8bb3b39821e40cc6c78ffd4fe93d7
Maiko Tan <maiko.tan.coding@gmail.com>
2024-08-03 14:09:37 +0800

    refa: remove override of import-x settings

--- typescript.js
+++ typescript.js
@@ -5,17 +5,6 @@ import ts from '@hamster-bot/eslint-config/typescript'
  */
 const config = [
   ...ts,
-  {
-    settings: {
-      'import-x/extensions': ['.cjs', '.js', '.mjs', '.jsx', 'cts', '.ts', '.mts', '.tsx'],
-      'import-x/parser': {
-        '@typescript-eslint/parser': ['.cts', '.ts', '.mts', '.tsx'],
-      },
-    },
-    rules: {
-      'import-x/no-unresolved': ['error'],
-    },
-  },
 ]
 
 export default config
