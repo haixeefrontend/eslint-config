1412fac7f11cc1c7e2cd98a75c6987a5ce4f3a3d
Maiko Tan <maiko.tan.coding@gmail.com>
2025-12-02 15:57:42 +0800

    refactor: enhance globals configuration for uni-app

--- rules/uniapp.js
+++ rules/uniapp.js
@@ -8,7 +8,22 @@ const config = [
   {
     languageOptions: {
       globals: {
-        uni: 'readonly',
+        uni: 'readonly', // uni-app global variable
+        plus: 'readonly', // h5 plus
+        UniApp: 'readonly',
+        UniNamespace: 'readonly',
+        // Mini Program globals
+        my: 'readonly', // Alipay
+        wx: 'readonly', // WeChat
+        ww: 'readonly', // WeWork
+        qq: 'readonly', // QQ
+        dd: 'readonly', // DingTalk
+        tt: 'readonly', // Bytedance / Toutiao / Lark / TikTok
+        swan: 'readonly', // Baidu
+        jd: 'readonly', // JD
+        xhs: 'readonly', // Xiaohongshu
+        // Vue globals
+        getCurrentPages: 'readonly',
       },
     },
     ignores: ['**/node_modules/**/*', '/dist/**/*', '**/unpackage/**/*'],
